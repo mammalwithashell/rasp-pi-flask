169dfa18d94567e86ab0449e83d78aaab29b256d
James <james.peters97@outlook.com>
2020-05-24 15:17:59 -0500

    backend completed

--- app_pkg/__init__.py
+++ app_pkg/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 
 # Name my flask instance "flask_app"
 flask_app = Flask(__name__)
+flask_app.debug = True
 
 # Import routes into the package
 from app_pkg import routes
--- app_pkg/routes.py
+++ app_pkg/routes.py
@@ -1,7 +1,15 @@
+# Import the flask instance from our package
 from app_pkg import flask_app
+from flask import render_template
 
+# Home route
 @flask_app.route("/")
 @flask_app.route("/index")
-def hello():
-	return "Hello World!"
+def index():
+	return render_template('hello.html', message="Hello World!")
 
+
+# Blog route
+@flask_app.route("/blog")
+def blog():
+	return "Hello Blog"
