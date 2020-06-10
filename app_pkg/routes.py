57c060b40f90335227f1463eb37ee0498a269d30
James <james.peters97@outlook.com>
2020-06-10 13:13:39 -0500

    Pictures and css styling

--- app_pkg/routes.py
+++ app_pkg/routes.py
@@ -2,14 +2,15 @@
 from app_pkg import flask_app
 from flask import render_template
 
+
 # Home route
 @flask_app.route("/")
-@flask_app.route("/index")
+@flask_app.route("/index", methods=['GET', 'POST'])
 def index():
-	return render_template('hello.html', message="Hello World!")
+    return render_template('hello.html')
 
 
 # Blog route
 @flask_app.route("/blog")
 def blog():
-	return "Hello Blog"
+    return "Hello Blog"
